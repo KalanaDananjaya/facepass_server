34b06c867c4d2ade2383d98d39d2e4b6f8ea4d19
Kalana <kalana.16@cse.mrt.ac.lk>
2019-02-05 12:38:23 +0530

    face verification with db endpoint created

--- src/server.js
+++ src/server.js
@@ -29,7 +29,7 @@ app.post('/register',registerUpload.single('file'),function(req,res){
     var sql ="INSERT INTO customer (email,firstName,uid) values(?,?,?)";
     var params=[email,firstName,uid];
     sql = mysql.format(sql,params);
-    //console.log(sql);
+    
     db.query(sql,function(err,result){
         if (err){
             throw err;
@@ -51,13 +51,8 @@ app.post('/register',registerUpload.single('file'),function(req,res){
         }
     })
     .then((response)=>{
-        console.log('******************555555555************************');
-        console.log(response.data);
-        console.log('********************55555*********************************');
-        
-        
-        var sql = "INSERT INTO faces (uid,vector) values(?,?)";
-        var params = [uid,JSON.stringify(response.data)];
+        var sql = "INSERT INTO faces (uid,face,vector) values(?,?,?)";
+        var params = [uid,fileName,JSON.stringify(response.data)]; //response.data is the vector of the face
 
         sql = mysql.format(sql,params);
         
@@ -68,6 +63,7 @@ app.post('/register',registerUpload.single('file'),function(req,res){
         }
         else{
             console.log("face vector query succesfully updated");
+            res.send(200);
         }
     });
         
@@ -79,7 +75,52 @@ app.post('/register',registerUpload.single('file'),function(req,res){
     
 });
 
+app.post('/verify',upload.single('file'),function(req,res){
+    console.log('recieved file for comparison');
+    var filename =req.file.filename; //obtain the file name of the uploaded file
+    var uid = req.body.uid;
+    var sql = "SELECT vector from faces where uid=?";
+    var params = [uid];
+    sql = mysql.format(sql,params);
+    
+    db.query(sql,function(err,result){
+        if (err){
+            throw err;
+        }
+        else{
+            console.log("face vector query succesfully recieved");
+            face_vector=result[0].vector;
 
+            var data = {
+                filename  : filename,
+                face_vector : face_vector
+            }
+        
+            axios.post('http://localhost:5000/verify',data,{
+                headers : {
+                    'Content-Type' : 'application/json'
+                }    
+            })
+            .then((res)=>{
+                console.log("cos similarity is");
+                console.log(res.data);
+            })
+            .catch((error)=>{
+                console.log("failure");
+                console.error(error);
+            })
+        }
+    });
+    
+    
+
+    
+});
+
+
+
+
+/*
 app.get('/',function(req,res){
    
     console.log('request is');
@@ -105,7 +146,8 @@ app.get('/',function(req,res){
     })
     res.send(200);
 });
-
+*/
+/*
 app.post('/upload',upload.single('file'),function(req,response){
     
     console.log('recieved file');
@@ -137,7 +179,6 @@ app.post('/upload',upload.single('file'),function(req,response){
     
     response.send("success");
 });
-
-
+*/
 
 app.listen(3000);
